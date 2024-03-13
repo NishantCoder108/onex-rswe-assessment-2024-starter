d155a68cbc8f4ab3573319d43598387b0d976704
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-13 18:23:05 +0530

    📝 carDetailsPage added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Navbar from "@/components/Navbar/Navbar";
-import Image from "next/image";
+import CarDetailsPage from "@/components/pages/CarDetailsPage/CarDetailsPage";
+import Navbar from "@/components/pages/Navbar/Navbar";
 
 export default function Home() {
     return (
         <>
             <Navbar />
+            <CarDetailsPage />
         </>
     );
 }

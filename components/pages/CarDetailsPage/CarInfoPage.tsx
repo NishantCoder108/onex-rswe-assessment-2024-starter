1f930055041de1ba085967eafaa0882aa8343724
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-14 12:28:44 +0530

    Optimize code

--- components/pages/CarDetailsPage/CarInfoPage.tsx
+++ components/pages/CarDetailsPage/CarInfoPage.tsx
@@ -1,11 +1,23 @@
+import StarRating from "@/components/common/StarRating";
+import { Badge } from "@/components/ui/badge";
 import React from "react";
 
 const CarInfoPage = () => {
     return (
-        <div>
-            <div className="font-graphic">Hello World</div>
+        <div className="  h-full pt-9 px-8">
+            <div className=" border-b border-[#BDBDBD] ">
+                <Badge className="bg-[#DDFFF7] text-[#0036C3] mb-3 hover:text-white">
+                    NEW
+                </Badge>
+                <p className="text-xl font-bold ">2021 Tesla Model 3</p>
+                <p className="text-xs my-1">
+                    <span className="text-gray-400 pr-3">VIN</span>{" "}
+                    JN1AZ0CPOBT009448
+                </p>
+                <StarRating rating={3.5} />
+                <p className=" font-semibold pb-4 pt-3">$41,103</p>
+            </div>
         </div>
     );
 };
-
 export default CarInfoPage;

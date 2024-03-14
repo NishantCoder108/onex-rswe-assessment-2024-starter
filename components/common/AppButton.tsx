29c3b20598d9f1a207e10a33d139b2f0fc9f4e23
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-14 21:04:52 +0530

    Implement common button

--- components/common/AppButton.tsx
+++ components/common/AppButton.tsx
@@ -1,56 +1,15 @@
-import { cva, VariantProps } from "class-variance-authority";
-import { ComponentProps } from "react";
-import { twMerge } from "tailwind-merge";
+import React from "react";
+import { Button } from "../ui/button";
 
-const buttonStyles = cva(
-    ["flex", "items-center", "justify-center", "transition-colors"],
-    {
-        variants: {
-            variant: {
-                default: ["bg-green-200", "hover:bg-green-100"],
-                hover: ["hover:bg-[#f1f5f980]"],
-                dark: ["bg-green-900", "hover:bg-slate-800", "text-white"],
-                gradient: [
-                    "bg-gradient-to-br from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500",
-                ],
-                hoverBorder: [
-                    "hover:border-4",
-                    "rounded-full",
-                    "p-2",
-                    "border-black",
-                ],
-                border: [
-                    "border-4",
-                    "rounded-full",
-                    "p-2",
-                    "border-black",
-                    "hover:bg-gradient-to-br",
-                    "hover:from-indigo-500",
-                    "hover:via-purple-500",
-                    "hover:to-pink-500",
-                ],
-            },
-            size: {
-                default: ["rounded", "w-20", "h-20", "p-2"],
-                button: ["rounded", "h-10", "w-32"],
-                icon: ["rounded-full", "w-20", "h-20", "p-2.5"],
-            },
-        },
-        defaultVariants: {
-            variant: "default",
-            size: "default",
-        },
-    }
-);
-
-type buttonType = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
-
-const AppButton = ({ variant, size, className, ...props }: buttonType) => {
+interface IButton {
+    btnText: string;
+    className?: string;
+}
+const AppButton = ({ btnText, className }: IButton) => {
     return (
-        <button
-            {...props}
-            className={twMerge(buttonStyles({ variant, size }), className)}
-        />
+        <div>
+            <Button className={className}>{btnText}</Button>
+        </div>
     );
 };
 

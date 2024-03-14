dc9aac9054c8a9de0e1acc69aced59dc6b7efe51
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-14 16:46:49 +0530

    Optimize code

--- components/pages/CarDetailsPage/CarInfoPage.tsx
+++ components/pages/CarDetailsPage/CarInfoPage.tsx
@@ -1,3 +1,4 @@
+import AppSelectColor from "@/components/common/AppSelectColor";
 import StarRating from "@/components/common/StarRating";
 import { Badge } from "@/components/ui/badge";
 import React from "react";
@@ -9,13 +10,18 @@ const CarInfoPage = () => {
                 <Badge className="bg-[#DDFFF7] text-[#0036C3] mb-3 hover:text-white">
                     NEW
                 </Badge>
-                <p className="text-xl font-bold ">2021 Tesla Model 3</p>
+                <p className="text-2xl font-bold ">2021 Tesla Model 3</p>
                 <p className="text-xs my-1">
                     <span className="text-gray-400 pr-3">VIN</span>{" "}
                     JN1AZ0CPOBT009448
                 </p>
-                <StarRating rating={3.5} />
-                <p className=" font-semibold pb-4 pt-3">$41,103</p>
+                <StarRating rating={5} />
+                <p className=" font-semibold text-lg pb-4 pt-3">$41,103</p>
+            </div>
+
+            <div>
+                <p className="font-bold my-4">Colour</p>
+                <AppSelectColor />
             </div>
         </div>
     );

38fc268626ba7d74678a7ba14518b181850b2d44
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-15 14:52:46 +0530

    minor

--- components/common/AppSkeleton.tsx
+++ components/common/AppSkeleton.tsx
@@ -33,15 +33,3 @@ const AppSkeleton = () => {
 };
 
 export default AppSkeleton;
-
-{
-    /* <div className="flex flex-col sm:flex-row">
-<div className="sm:w-4/6 w-full bg-[#FBFCFE] ">
-    <CarImgCarousel carCarousel={carCarouselList} />
-</div>
-
-<div className="sm:w-1/3 w-full mt-4 sm:mt-0">
-    <CarInfoPage carInfo={carInfoDetails} />
-</div>
-</div> */
-}

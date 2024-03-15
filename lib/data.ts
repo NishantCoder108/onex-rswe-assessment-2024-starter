6c586a0225a93bf90e5d3505a7ef738623df3e0e
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-15 11:42:34 +0530

    add mock data

---
+++ lib/data.ts
@@ -0,0 +1,30 @@
+export const specificationData = {
+    Exterior: {
+        ["Body Style"]: "Hatchback",
+        ["Exterior Colour"]: "Blue",
+        ["Length"]: "175″",
+        ["Wheelbase"]: "106″",
+        ["Width"]: "69.7″",
+        ["Clearance"]: "6″",
+        ["Full Width"]: "N/A",
+    },
+    Interior: {
+        ["Seating"]: "5 adults",
+        ["Interior Colour"]: "N/A",
+        ["Head Room Front"]: "41″",
+        ["Head Room Rear"]: "37″",
+        ["Leg Room Front"]: "42″",
+        ["Leg Room Rear"]: "33″",
+        ["Shoulder Room Front"]: "54″",
+    },
+    ["BEV Performance"]: {
+        ["Horsepower"]: "107 hp",
+        ["MPGe"]: "102",
+        ["Top Speed"]: "90 mph",
+    },
+    Charging: {
+        ["Electric Range"]: "73 Mi.",
+        ["Battery Size"]: "24 kWh",
+        ["Cost to charge (full)"]: "$2.50",
+    },
+};
